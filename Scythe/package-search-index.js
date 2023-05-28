2a7eef98198e1cb335598e3acdaec8277ab407b4
RhythmicSys <mc.adhdmc@gmail.com>
2023-05-28 16:02:16 -0700

    Add Scythe docs to site

---
+++ Scythe/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"simplexity.scythe"},{"l":"simplexity.scythe.commands"},{"l":"simplexity.scythe.commands.subcommands"},{"l":"simplexity.scythe.config"},{"l":"simplexity.scythe.events"},{"l":"simplexity.scythe.hooks"},{"l":"simplexity.scythe.interactions"}];updateSearchResults();
